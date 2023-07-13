5527ba02d8da7c225ab478f1a350b234a762b19a
Federico Giuliani <federicogiuliani@Federicos-MacBook-Air.local>
2023-07-13 18:26:18 -0300

    up README

---
+++ README.md
@@ -0,0 +1,10 @@
+<div>
+<h1>Proyecto Final Backend 43355</h1>
+<p> Este proyecto fue desarrollado con las siguientes librerias:</p>
+    <li>
+    <ul>Node.JS</ul>
+    <ul>Express</ul>
+    <ul>Handlebars</ul>
+    <ul>Mongoose</ul>
+    <ul>Nodemon</ul>
+    </li>
--- public/main.js
+++ public/main.js
@@ -1,46 +1,4 @@
 
-
-/*// Escuchar evento 'updateProducts' y actualizar la lista de productos
-socket.on('updateProducts', (products) => {
-    // Actualizar la lista en la vista home
-    if (window.location.pathname === '/') {
-        updateProductList(products);
-    }
-    // Actualizar la lista en la vista realTimeProducts
-    if (window.location.pathname === '/realtimeproducts') {
-        updateProductList(products);
-    }
-});
-
-// Función para actualizar la lista de productos en la vista
-function updateProductList(products) {
-    const productList = document.querySelector('#product-list');
-    productList.innerHTML = '';
-
-    for (const product of products) {
-        const listItem = document.createElement('li');
-        listItem.textContent = product.title;
-        productList.appendChild(listItem);
-    }
-}
-
-function removeProduct(productId) {
-    fetch(`/products/${productId}`, {
-        method: 'DELETE'
-    })
-        .then(response => {
-            if (response.ok) {
-                console.log('Producto eliminado');
-            }
-        })
-        .catch(error => {
-            console.error('Error al eliminar el producto:', error);
-        });
-}
-*/
-// Importa el CartManager si es necesario
-
-// Obtén una referencia al botón
 const addProductButton = document.getElementById('addProductButton');
 
 addProductButton.addEventListener('click', () => {
@@ -62,16 +20,3 @@ addProductButton.addEventListener('click', () => {
         })
         .catch(error => console.log(error));
 });
-
-// Agrega un evento de clic al botón
-/*addProductButton.addEventListener('click', () => {
-    // Obtén el ID del producto (puedes obtenerlo del DOM o pasarlo como parámetro)
-    const productId = addProductButton.dataset.productId;;
-    if (productId) {
-        console.log(productId)
-    }
-    else {
-        console.log('no hay id')
-    }
-    console.log('Producto agregado al carrito:', cart);
-});*/
